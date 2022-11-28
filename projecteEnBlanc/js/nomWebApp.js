6dc6c1b791b7864ff367a5bdbe5c284cdd7cba79
JesusCasablanca-examen <jcasablancai2223@politecnics.barcelona>
2022-11-28 09:59:37 +0100

    botones

--- projecteEnBlanc/js/nomWebApp.js
+++ projecteEnBlanc/js/nomWebApp.js
@@ -30,3 +30,52 @@ function activar2(){
    }
   
 }
+
+function activar3(){
+   //document.getElementById("caja1").style.display = 'initial';
+   //estado.classList.remove('prgrOff'); 
+
+   
+
+   if(document.getElementById("caja3").style.display == 'none'){
+      document.getElementById("caja3").style.display = 'block';
+
+   }
+   else{
+      document.getElementById("caja3").style.display = 'none';
+   }
+  
+}
+
+function activar4(){
+   //document.getElementById("caja1").style.display = 'initial';
+   //estado.classList.remove('prgrOff'); 
+
+   
+
+   if(document.getElementById("caja4").style.display == 'none'){
+      document.getElementById("caja4").style.display = 'block';
+
+   }
+   else{
+      document.getElementById("caja4").style.display = 'none';
+   }
+  
+}
+
+
+function activar5(){
+   //document.getElementById("caja1").style.display = 'initial';
+   //estado.classList.remove('prgrOff'); 
+
+   
+
+   if(document.getElementById("caja5").style.display == 'none'){
+      document.getElementById("caja5").style.display = 'block';
+
+   }
+   else{
+      document.getElementById("caja5").style.display = 'none';
+   }
+  
+}
